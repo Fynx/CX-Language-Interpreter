1298d51123125cde3f724a7d07e8332aaad74ae6
Piotr Majcherczyk <fynxor@gmail.com>
2016-06-07 18:28:53 +0200

    Add skeleton for type checking expressions and statements.

--- CXTypeChecking.hs
+++ CXTypeChecking.hs
@@ -31,28 +31,100 @@ type TypeCont = (Env, TStore, FunArgs, RetType)
 type TES a = ExceptT String (StateT TypeCont (IO)) a
 
 
+-- Declarations
+
+ctDecl :: Decl -> TES ()
+ctDecl (DeclDefault ts ids) = return ()
+ctDecl (DeclDefine ts id e) = do
+    et <- ctExp e
+    if ts == et
+      then return ()
+      else throwError $ "Attempting to assign invalid value to variable " ++ showId id
+
+
+-- Expressions
+
+ctExp :: Exp -> TES TypeSpec
+ctExp (ExpAssign e1 op e2) = return TypeVoid
+ctExp (ExpCondition cond e1 e2) = return TypeVoid
+ctExp (ExpLogOr e1 e2) = return TypeVoid
+ctExp (ExpEq e1 e2) = return TypeVoid
+ctExp (ExpNeq e1 e2) = return TypeVoid
+ctExp (ExpLt e1 e2) = return TypeVoid
+ctExp (ExpGt e1 e2) = return TypeVoid
+ctExp (ExpLe e1 e2) = return TypeVoid
+ctExp (ExpGe e1 e2) = return TypeVoid
+ctExp (ExpAdd e1 e2) = return TypeVoid
+ctExp (ExpSub e1 e2) = return TypeVoid
+ctExp (ExpMul e1 e2) = return TypeVoid
+ctExp (ExpDiv e1 e2) = return TypeVoid
+ctExp (ExpMod e1 e2) = return TypeVoid
+ctExp (ExpUnaryInc e) = return TypeVoid
+ctExp (ExpUnaryDec e) = return TypeVoid
+ctExp (ExpPostInc uop e) = return TypeVoid
+ctExp (ExpFuncP e) = return TypeVoid
+ctExp (ExpFuncPArgs e args) = return TypeVoid
+ctExp (ExpConstant c) = return TypeVoid
+
+
 -- Statements
 
+
+ctStmt :: Stmt -> TES ()
+ctStmt (StmtExp e) = do
+    _ <- ctExp e
+    return ()
+ctStmt (StmtCompound stmt) = ctCompoundStmt stmt
+ctStmt (StmtSelection stmt) = ctSelectionStmt stmt
+ctStmt (StmtIteration stmt) = ctIterationStmt stmt
+ctStmt (StmtReturn e) = ctReturnStmt e
+ctStmt (StmtDecl d) = ctDecl d
+
+
 ctCompoundStmt :: CompoundStmt -> TES ()
-ctCompoundStmt cs = return ()
+ctCompoundStmt (StmtCompoundEmpty) = return ()
+ctCompoundStmt (StmtCompoundList stmts) = do
+    mapM_ ctStmt stmts
+    return ()
+
+ctSelectionStmt :: SelectionStmt -> TES ()
+ctSelectionStmt (StmtIf e stmt) = do
+    et <- ctExp e
+    case et of
+      TypeBool -> return () --TODO make it pretty
+      otherwise -> throwError $ "Non-boolean value in 'if' condition " ++ show e
+    ctCompoundStmt stmt
+
+ctIterationStmt :: IterationStmt -> TES ()
+ctIterationStmt s = return () --TODO
+
+ctReturnStmt :: Exp -> TES ()
+ctReturnStmt e = do
+    rtype <- ctExp e
+    (env, tstore, fargs, _) <- lift get
+    lift $ put $ (env, tstore, fargs, rtype)
+    return ()
+
+
+-- Functions
 
 
 ctFunction :: (Ident, Loc) -> TES ()
 ctFunction (id, loc) = do
     (env, tstore, fargs, _) <- lift get
-    liftIO $ putStrLn $ show id
     case Map.lookup loc fargs of
       Nothing -> do
-        liftIO $ putStrLn $ "Not a function: " ++ show id
+        liftIO $ putStrLn $ "Not a function: " ++ showId id
         return () -- Not a function
       Just (ts, args, stmt) -> do
-        liftIO $ putStrLn $ "Check function " ++ show id ++ " with type " ++ showTS ts ++
+        liftIO $ putStrLn $ "Check function " ++ showId id ++ " with type " ++ showTS ts ++
                             " and args " ++ show args ++ "\n"
         _ <- ctCompoundStmt stmt
         (_, _, _, rtype) <- lift get
+        lift $ put $ (env, tstore, fargs, TypeVoid)
         if rtype == ts
           then return ()
-          else throwError $ "Invalid return value of function " ++ show id ++
+          else throwError $ "Invalid return value of function " ++ showId id ++
                             "\n  Expected type: " ++ showTS ts ++ "\n  Actual type:   " ++
                             showTS rtype
 
@@ -62,6 +134,10 @@ showTS TypeInt    = "Int"
 showTS TypeVoid   = "Void"
 showTS TypeString = "String"
 showTS TypeBool   = "Bool"
+
+--TODO move somewhere else
+showId :: Ident -> String
+showId (Ident s) = s
 
 
 checkTypes :: TES ()
